377dbac5c7b1f940c5d8adc58c02d04aa892788c
KMJ217 <kmj217@hotmail.com>
2019-08-28 22:32:56 -0400

    updating heroku

--- server.js
+++ server.js
@@ -15,7 +15,14 @@ app.set('view engine', 'handlebars');
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/Article";
 
-mongoose.connect(MONGODB_URI);
+mongoose.connect(MONGODB_URI, function(err) {
+  if (err) {
+      console.log(err);
+  }
+  else {
+      console.log("Mongoose connection is successful.");
+  }
+});
 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
